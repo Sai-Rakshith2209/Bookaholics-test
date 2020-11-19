ed35e954a7ea673da2420cb8f98d2a9a4ad0e479
Sai Rakshith <sairakshith10@gmail.com>
2020-11-19 10:04:08 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,69 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Mastering Cloud Computing",
+    img: "img/img-pro-01.jpg",
+    price: 100,
+    company: "ABC Publication",
+    info:
+      "Authored by veterans in the subject field, this new textbook on Cloud Computing explores the most critical cloud components and covers all crucial aspects of the subject like Parallel and Distributed Computing, Virtualization, Cloud Computing Architecture, Cloud Platforms in Industry and its Applications. With its student-accessible vocabulary and writing style, the book will surely prove to be an excellent and enjoyable read!",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: "Tamil Nadu",
+    img: "img/img-pro-02.jpg",
+    price: 16,
+    company: "ABC Publications",
+    info:
+      "The gateway to South India, the state of Tamil Nadu is an archive of ancient Dravidian legacy with a distinctive modernity. Known as the true soul of the south, this enchanting land beckons the tourist with one of the architectural wonders of India – the Meenakshi Temple in Madurai, the great Hindu pilgrim centres of Kancheepuram and Mamallapuram and splendour of the Chidambaram Temple. The holy town of Rameswaram, the coastal area with beautiful beach sites at Kovalam, Kanniyakumari and Vattakottai, the crowning glory of exotic hill stations of Ooty (Udhagamandalam), Kodaikanal and Yercaud, sanctuaries with exotic wildlife and the gurgling waterfalls of Hogenekkal attract the tourists.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 3,
+    title: "Indian Cooking",
+    img: "img/img-pro-03.jpg",
+    price: 900,
+    company: "Cooking",
+    info:
+      "Classic recipes from the many regions of a diverse and culturally rich nation. Indian cuisine is widely revered for its distinctive tastes and ingredients. The sheer size of India, combined with its history and religious diversity, is reflected in the country's cuisine. Complete Book of Indian Cooking provides hundreds of user-friendly and great-tasting recipes from all regions and cultures of this extraordinary country. Local/regional ingredients deliver unusual flavors that satisfy the search for new tastes. Each recipe includes background information that helps the home cook become more knowledgeable about -- and comfortable with -- Indian food.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 4,
+    title: "The Upside Down King",
+    img: "img/img-pro-04.jpg",
+    price: 18,
+    company: "htc",
+    info:
+      "Did you know there was a time when bears spoke, the moon laughed and babies were found inside fish? The tales in this collection surround the two most popular avatars of Lord Vishnu-Rama and Krishna-and their lineage. Countless stories about the two abound, yet most are simply disappearing from the hearts and minds of the present generation. Bestselling author Sudha Murty takes you on an arresting tour, all the while telling you of the days when demons and gods walked alongside humans, animals could talk and gods granted the most glorious boons to common people.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0,
+};
